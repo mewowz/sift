1e926a34391450b39f431dc838b5962e60edb805
mewowz <mewowz@lainx.org>
2026-08-12 05:07:16 -0700

    core: add StateEstimator concept
    - Exists to generalize the very common predict/update steps that filters
      have without assuming much more about the filter itself

---
+++ include/sift/core/filter_concepts.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <concepts>
+
+/* 
+* StateEstimator is a concept because we don't want to assume that
+* every estimator has the same shape, members, or steps to it. However, 
+* basically every estimator is a predict/update loop. Nearly 
+* everything should be able to be implemented based on this
+* general concept at the very least
+*/
+
+namespace sift::core {
+
+template <typename F>
+concept StateEstimator = requires(F f) {
+    typename F::state_type;
+    typename F::measurement_type;
+    { f.predict() } -> std::same_as<void>;
+    { f.update(std::declval<typename F::measurement_type>()) } -> std::same_as<void>;
+    { f.state() } -> std::convertible_to<typename F::state_type>;
+};
+
+}  // namespace sift::core
